95319be44ce2a97dfb13709c70eb9e1432b6e48d
gonglinjie <gonglinjie@worktile.com>
2015-08-14 17:06:17 +0800

    init

---
+++ src/main/java/jenkins/plugins/slack/StandardSlackService.java
@@ -0,0 +1,104 @@
+package jenkins.plugins.slack;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.methods.PostMethod;
+
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import jenkins.model.Jenkins;
+import hudson.ProxyConfiguration;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
+
+public class StandardSlackService implements SlackService {
+
+    private static final Logger logger = Logger.getLogger(StandardSlackService.class.getName());
+
+    private String host = "slack.com";
+    private String teamDomain;
+    private String token;
+    private String[] roomIds;
+
+    public StandardSlackService(String teamDomain, String token, String roomId) {
+        super();
+        this.teamDomain = teamDomain;
+        this.token = token;
+        this.roomIds = roomId.split("[,; ]+");
+    }
+
+    public boolean publish(String message) {
+        return publish(message, "warning");
+    }
+
+    public boolean publish(String message, String color) {
+        boolean result = true;
+        String url = teamDomain;
+        HttpClient client = getHttpClient();
+        PostMethod post = new PostMethod(url);
+        JSONObject json = new JSONObject();
+
+        try {
+            JSONObject field = new JSONObject();
+            field.put("short", false);
+            field.put("value", message);
+
+            JSONArray fields = new JSONArray();
+            fields.put(field);
+
+            JSONObject attachment = new JSONObject();
+            attachment.put("fallback", message);
+            attachment.put("color", color);
+            attachment.put("fields", fields);
+            JSONArray attachments = new JSONArray();
+            attachments.put(attachment);
+            json.put("attachments", attachments);
+
+            post.addParameter("payload", json.toString());
+            post.getParams().setContentCharset("UTF-8");
+            int responseCode = client.executeMethod(post);
+            String response = post.getResponseBodyAsString();
+            if(responseCode != HttpStatus.SC_OK) {
+                logger.log(Level.WARNING, "Slack post may have failed. Response: " + response);
+                result = false;
+            }
+        } catch (Exception e) {
+            logger.log(Level.WARNING, "Error posting to Slack", e);
+            result = false;
+        } finally {
+            logger.info("Posting succeeded");
+            post.releaseConnection();
+        }
+        return result;
+    }
+
+    protected HttpClient getHttpClient() {
+        HttpClient client = new HttpClient();
+        if (Jenkins.getInstance() != null) {
+            ProxyConfiguration proxy = Jenkins.getInstance().proxy;
+            if (proxy != null) {
+                client.getHostConfiguration().setProxy(proxy.name, proxy.port);
+                String username = proxy.getUserName();
+                String password = proxy.getPassword();
+                // Consider it to be passed if username specified. Sufficient?
+                if (username != null && !"".equals(username.trim())) {
+                    logger.info("Using proxy authentication (user=" + username + ")");
+                    // http://hc.apache.org/httpclient-3.x/authentication.html#Proxy_Authentication
+                    // and
+                    // http://svn.apache.org/viewvc/httpcomponents/oac.hc3x/trunk/src/examples/BasicAuthenticationExample.java?view=markup
+                    client.getState().setProxyCredentials(AuthScope.ANY,
+                        new UsernamePasswordCredentials(username, password));
+                }
+            }
+        }
+        return client;
+    }
+
+    void setHost(String host) {
+        this.host = host;
+    }
+}
